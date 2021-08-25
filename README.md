3b19c75b0fe0d5fa8a461472006ee71970bdbf1e
Andrei way <andreicho@Andreis-MacBook-Pro.local>
2021-08-25 18:08:45 -0400

    fixed site now its working and added imgs and more to read me

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # HW-4
+added java and html 
+code works now have to refresh page to if you want to redo quiz no gliches that way 
+
+![websit](imgs/img-1.jpg)
+![websit with differnt correct answer](imgs/img-2.jpg) 
--- asseset /script.js
+++ asseset /script.js
@@ -1,16 +1,17 @@
 const startQuiz = document.getElementById("start-quiz")
-const answerForm = document.getElementById("answer-form")
-const timmer = document.getElementById("timmer")
+const answerForm = document.getElementById("answers-form")
+const timmer = document.getElementById("timer")
 const nextButton = document.getElementById("next")
 const quitButton = document.getElementById("quit")
-var startTime = 3
+var startTime = 50
+const storage = window.localStorage
 
 
 const questionTitle = document.getElementById("question-title")
-const answer1Lable = document.getElementById("answer-1-lable")
-const answer2Lable = document.getElementById("answer-2-lable")
-const answer3Lable = document.getElementById("answer-3-lable")
-const answer4Lable = document.getElementById("answer-4-lable")
+const answer1Label = document.getElementById("answer-1-label")
+const answer2Label = document.getElementById("answer-2-label")
+const answer3Label = document.getElementById("answer-3-label")
+const answer4Label = document.getElementById("answer-4-label")
 const answer1 = document.getElementById("answer-1")
 const answer2 = document.getElementById("answer-2")
 const answer3 = document.getElementById("answer-3")
@@ -19,7 +20,7 @@ const answer4 = document.getElementById("answer-4")
 const questions = [
     {
         question: "What is 2 + 2?",
-        answerNum: 4,
+        answerNum: 2,
         one: "2",
         two: "4",
         three: "9",
@@ -28,7 +29,7 @@ const questions = [
 
     {
         question: "What is 2 * 6 ?",
-        answerNum: 12,
+        answerNum: 4,
         one: "2",
         two: "4",
         three: "9",
@@ -36,7 +37,7 @@ const questions = [
     },
     {
         question: "What is 2 + 7?",
-        answerNum: 9,
+        answerNum: 3,
         one: "2",
         two: "4",
         three: "9",
@@ -44,7 +45,7 @@ const questions = [
     },
     {
         question: "What is 2 + 0?",
-        answerNum: 2,
+        answerNum: 1,
         one: "2",
         two: "4",
         three: "9",
@@ -56,137 +57,152 @@ questionTitle.style.display = "none"
 answerForm.style.display = "none"
 timmer.style.display = "none"
 
-function quiz() {
+//quiz function
+function quiz(){
     questionTitle.style.display = "initial"
     answerForm.style.display = "initial"
-    timmer.style.display = "initial"
+    timer.style.display = "initial"
 
-    questionNum = 0
 
-    questionTool = questions.length
+    //initialize question num counter to keep track of "questions" array index
+    let questionNum = 0
 
+
+    //intialize total amount of questions to keep track of 
+    let questionTotal = questions.length
+
+
+    //initialize total amount of questions correct
     let correctAnswers = 0
 
-    questions.forEach(question => {
-        questionTitle.innerText = question.question
-        answer1Lable.innerText = question.one
-        answer2Lable.innerText = question.two
-        answer3Lable.innerText = question.three
-        answer4Lable.innerText = question.four
-        console.log("starquiz")
-      //let nextQuestion= true
 
-        while (nextQuestion) {
-            nextButton.addEventListener("click", (e) => {
-                e.preventDefault()
-                if (question.answerNum === 1 &&
-                    answer1.checked === true
-                ) {
-                    correctAnswer++
-                    nextQuestion = true
-                }
-console.dir(quiz)
-            })
+    //initialize amount of time
+    let startTime = 50
+
+
+    //initialize first question
+    questionTitle.innerText = questions[0].question
+    answer1Label.innerText = questions[0].one
+    answer2Label.innerText = questions[0].two
+    answer3Label.innerText = questions[0].three
+    answer4Label.innerText = questions[0].four
+
+
+    //check timer
+    function checkTimer(){
+        if(startTime === 0){
+            questionTitle.innerText = `You did not complete the quiz in time`
+            answerForm.style.display = "none"
+            timer.style.display = "none"
+            startQuiz.style.display = "initial"
+        }
+    }
+
+
+    //starting timer
+    setInterval(() => {
+        startTime--
+        timer.innerText = `${startTime} Seconds Left`
+        checkTimer()
+    }, 1000)
+
+
+    //decrease time function
+    function decreaseTimer(){
+        startTime -= 10
+    }
+
+
+    //go to next question
+    function stageNextQuestion(index, correct){
+        console.log(`Index: ${index} Correct Answers: ${correctAnswers}`)
+
+
+        //if the answer is correct, increase the number of correct answers
+        correct ? correctAnswers++ : decreaseTimer()
+
+
+        //if there are no more questions, bring up some text and the start button
+        if(index === (questionTotal)){
+            console.log('quiz completed')
+            questionTitle.innerText = `You have completed the quiz with a score of ${(correctAnswers / questionTotal) * 100}%`
+            answerForm.style.display = "none"
+            timer.style.display = "none"
+            startQuiz.style.display = "initial"
+            return 
+        }
+
+
+        //stage the next question to be answered
+        console.log(`Question Number ${index + 1} after question ${index} was ${correct}`)
+        questionTitle.innerText = questions[index].question
+        answer1Label.innerText = questions[index].one
+        answer2Label.innerText = questions[index].two
+        answer3Label.innerText = questions[index].three
+        answer4Label.innerText = questions[index].four
+    }
+
+
+    //create an event listener on the submission of the form, which will fire when the 
+    //next button is pressed
+    answerForm.addEventListener('submit', (e) => {
+        e.preventDefault()
+
+
+        let question = questions[questionNum]
+        console.log(question.answerNum)
+
+
+        //find out what the correct answer is according to the question object, and then
+        //check to see if the user selected that question
+        switch (question.answerNum){
+            case 1:
+                //change the question number that we're on since they selected an answer
+                questionNum++
+                
+                //if they were correct, go to the next question and mark it correct, otherwise, mark it wrong
+                answer1.checked ? stageNextQuestion(questionNum, true) : stageNextQuestion(questionNum, false)
+                break;
+            
+            case 2:
+                questionNum++
+                answer2.checked ? stageNextQuestion(questionNum, true) : stageNextQuestion(questionNum, false)
+                break;
+
+
+            case 3:
+                questionNum++
+                answer3.checked ? stageNextQuestion(questionNum, true) : stageNextQuestion(questionNum, false)
+                break;
+
+
+            case 4:
+                questionNum++
+                answer4.checked ? stageNextQuestion(questionNum, true) : stageNextQuestion(questionNum, false)
+                break;
+
+
+            default:
+                alert("Please select an answer")
         }
     })
-}
 
-startQuiz.addEventListener("click", (e) => {
-    e.preventDefault()
-    e.target.style.display = "none"
-    quiz()
-})
 
-function checkTimmer() {
-    if (startTime === 0) {
-        questionTitle.innerText = `You did not complete the quiz in time`
-        answerForm.style.display = "none"
-        timmer.style.display = "none"
-        startQuiz.style.display = "initial"
-        clearInterval(intervalId)
-    }
-}
-
-    var intervalId = setInterval(() => {
-    startTime--
-    timmer.innerText = `${startTime} Seconds Left`
-    checkTimmer()
-}, 1000);
-
-function decreaseTimer() {
-    startTime - 10
-}
-function stageNextQuestion(index,correct) {
-    console.log(`Index: ${index} Correct Answers: ${correctAnswers}`)
-
-    correct ? correctAnswers++ : decreaseTimer()
-
-    if (index === (questionTotal)) {
-        console.log('quiz completed')
-        questionTitle.innerText = `You have completed the quiz with a score of ${(correctAnswers / questionTotal) * 100}%`
+    quitButton.addEventListener('click', (e) => {
+        e.preventDefault()
+        questionTitle.style.display = "none"
         answerForm.style.display = "none"
         timer.style.display = "none"
         startQuiz.style.display = "initial"
-        return
-    }
-
-    console.log(`Question Number ${index + 1} after question ${index} was ${correct}`)
-    questionTitle.innerText = questions[index].question
-    answer1Lable.innerText = questions[index].one
-    answer2Lable.innerText = questions[index].two
-    answer3Lable.innerText = questions[index].three
-    answer4Lable.innerText = questions[index].four
-
+    })
 }
-
-answerForm.addEventListener('submit', (e) => {
-    e.preventDefault()
-    console.log('submitted')
-
-    let question = questions [questionNum]
-    console.log(question.answerNum)
-
-    switch(question.answerNum){
-        case 1:
-
-            questionNum++
-
-            answer1.checked ? stageNextQuestion(questionNum, true) : stageNextQuestion(questionNum, false)
-            break;
-
-        case 2:
-            questionNum++
-            answer2.checked ? stageNextQuestion(questionNum, true) : stageNextQuestion(questionNum, false)
-            break;
-
-        case 3:
-            questionNum++
-            answer3.checked ? stageNextQuestion(questionNum, true) : stageNextQuestion(questionNum, false)
-            break;
-
-        case 4:
-            questionNum++
-            answer4.checked ? stageNextQuestion(questionNum, true) : stageNextQuestion(questionNum, false)
-            break;
-
-        default:
-            alert("Please select an answer")
-    }
-})
-
-quitButton.addEventListener('click', (e) => {
-    e.preventDefault()
-    questionTitle.style.display = "none"
-    answerForm.style.display = "none"
-    timer.style.display = "none"
-    startQuiz.style.display = "initial"
-})
-
-
-
+//when you start the quiz, the first answer will pop up
 startQuiz.addEventListener('click', (e) => {
     e.preventDefault()
     e.target.style.display = "none"
 
     quiz()
 })
+
+
+
